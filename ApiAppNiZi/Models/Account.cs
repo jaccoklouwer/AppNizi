ab128fcc133a37218122640166114bf3cceff532
jaccoklouwer <jacco1994@hotmail.com>
2019-10-02 14:08:56 +0200

    resolved merge conflict

---
+++ ApiAppNiZi/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Account
+{
+	public Account()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
