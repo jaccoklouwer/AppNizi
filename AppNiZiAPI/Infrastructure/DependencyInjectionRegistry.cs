6ad175b9d248de48f3b9d822f52038e091fec176
jaccoklouwer <jacco1994@hotmail.com>
2019-10-28 12:35:51 +0100

    dependencyInjection for dietaryManagment

--- AppNiZiAPI/Infrastructure/DependencyInjectionRegistry.cs
+++ AppNiZiAPI/Infrastructure/DependencyInjectionRegistry.cs
@@ -17,6 +17,7 @@ namespace AppNiZiAPI.Infrastructure
             services.AddSingleton<IFoodRepository, FoodRepository>();
 
             services.AddSingleton<IMealRepository, MealRepository>();
+            services.AddSingleton<IDietaryManagementRepository, DietaryManagementRepository>();
 
             return services;
         }
